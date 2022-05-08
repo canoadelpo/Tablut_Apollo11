e7bf283d9f2507d56bd91406019491ef48b7350e
Giu999 <73532107+Giu999@users.noreply.github.com>
2022-05-08 12:04:28 +0200

    Aggiornamento runmyplayer.sh

--- Executables/runmyplayer.sh
+++ Executables/runmyplayer.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-java -jar /home/tablut/tablut/RandomPlayer.jar "$1" "$2" "$3"
+if [[ $# -ne ]] ; then
+  echo "Servono tutti gli argomenti"
+  echo "Inserire WHITE o BLACK come primo parametro"
+  echo "Il timeout in secondi come secondo parametro"
+  echo "L'ip del server come terzo parametro"
+  echo "Ad esempio: $0 WHITE 60 192.168.20.254"
+  exit 1
+fi
+
+java -jar /home/tablut/Tablut_Apollo11/apollo11.jar "$1" "$2" "$3"
